faae19ebaa2e603c5705f67efb89bd096324b60c
SantaDrugen <emil0510@hotmail.com>
2023-12-07 12:28:23 +0100

    Logic Interface switchup -- getUserInformation moved

--- UserStack/src/main/java/UserSystemStack/BusinessTier/Application/Logic/Interfaces/IUserInfoLogic.java
+++ UserStack/src/main/java/UserSystemStack/BusinessTier/Application/Logic/Interfaces/IUserInfoLogic.java
@@ -1,7 +1,10 @@
 package UserSystemStack.BusinessTier.Application.Logic.Interfaces;
 
+import UserSystemStack.Shared.DTOs.UserInfoDto;
+
 public interface IUserInfoLogic {
 
     boolean userExists(String username);
 
+    UserInfoDto getUserInformation(String username);
 }
